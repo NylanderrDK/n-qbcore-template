ef64ba02986fbd91dcce034dae3514943d290f5c
NylanderrDK <hjaltenylander@gmail.com>
2022-01-22 12:57:39 +0100

    fix(qb-smallresources): remove thread for lj-fuel

--- resources/[qb]/qb-smallresources/client/ignore.lua
+++ resources/[qb]/qb-smallresources/client/ignore.lua
@@ -61,30 +61,6 @@ if Config.IdleCamera then --Disable Idle Cinamatic Cam
 end
 
 CreateThread(function()
-    while true do
-        local ped = PlayerPedId()
-        local weapon = GetSelectedPedWeapon(ped)
-		if weapon ~= `WEAPON_UNARMED` then
-			if IsPedArmed(ped, 6) then
-				DisableControlAction(1, 140, true)
-				DisableControlAction(1, 141, true)
-				DisableControlAction(1, 142, true)
-			end
-
-			if weapon == `WEAPON_FIREEXTINGUISHER` or  weapon == `WEAPON_PETROLCAN` then
-				if IsPedShooting(ped) then
-					SetPedInfiniteAmmo(ped, true, `WEAPON_FIREEXTINGUISHER`)
-					SetPedInfiniteAmmo(ped, true, `WEAPON_PETROLCAN`)
-				end
-			end
-		else
-			Wait(500)
-		end
-        Wait(7)
-    end
-end)
-
-CreateThread(function()
     local pedPool = GetGamePool('CPed')
     for k,v in pairs(pedPool) do
         SetPedDropsWeaponsWhenDead(v, false)
