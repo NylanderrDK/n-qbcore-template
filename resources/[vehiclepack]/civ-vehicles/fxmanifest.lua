3cf2196e6bb055fbb5c47ea96348fec292af0f62
NylanderrDK <hjaltenylander@gmail.com>
2022-01-22 14:47:58 +0100

    feat(vehicles): add new civilian vehicles

---
+++ resources/[vehiclepack]/civ-vehicles/fxmanifest.lua
@@ -0,0 +1,70 @@
+fx_version "cerulean"
+game "gta5"
+
+-- NSPEEDO
+files {
+    "meta/nspeedo/*.meta",
+    "audio/nspeedo/nspeedo_game.dat151",
+    "audio/nspeedo/nspeedo_game.dat151.nametable",
+    "audio/nspeedo/nspeedo_game.dat151.rel",
+    "audio/nspeedo/nspeedo_sounds.dat54",
+    "audio/nspeedo/nspeedo_sounds.dat54.nametable",
+    "audio/nspeedo/nspeedo_sounds.dat54.rel",
+    "audio/nspeedo/sfx/dlc_nspeedo/nspeedo.awc",
+    "audio/nspeedo/sfx/dlc_nspeedo/nspeedo_npc.awc"
+}
+
+data_file "HANDLING_FILE" "meta/nspeedo/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "meta/nspeedo/vehicles.meta"
+data_file "CARCOLS_FILE" "meta/nspeedo/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/nspeedo/carvariations.meta"
+--
+data_file "AUDIO_GAMEDATA" "audio/nspeedo/nspeedo_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/nspeedo/nspeedo_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/nspeedo/sfx/dlc_nspeedo"
+
+-- MULEF
+files {
+    "meta/mulef/*.meta",
+    "audio/mulef/mulef_game.dat151.rel"
+}
+
+data_file "HANDLING_FILE" "meta/mulef/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "meta/mulef/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/mulef/carvariations.meta"
+--
+data_file "AUDIO_GAMEDATA" "audio/mulef/mulef_game.dat"
+
+-- BISONUTIL
+files {
+    "meta/bisonutil/*.meta",
+}
+
+data_file "HANDLING_FILE" "meta/bisonutil/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "meta/bisonutil/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/bisonutil/carvariations.meta"
+
+-- SURGE2
+files {
+    "meta/surge2/*.meta",
+}
+
+data_file "HANDLING_FILE" "meta/surge2/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "meta/surge2/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/surge2/carvariations.meta"
+
+-- WARRENER
+files {
+    "meta/warrener/*.meta",
+}
+
+data_file "VEHICLE_VARIATION_FILE" "meta/warrener/carvariations.meta"
+data_file "CARCOLS_FILE" "meta/warrener/carcols.meta"
+
+-- DILETTANTE
+files {
+    "meta/dilettante/*.meta",
+}
+
+data_file "VEHICLE_VARIATION_FILE" "meta/dilettante/carvariations.meta"
+data_file "CARCOLS_FILE" "meta/dilettante/carcols.meta"
